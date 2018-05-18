80dbc38204a475582b3d6dafa7ca7be5e6c92a78
EMCain <>
2018-05-18 15:29:45 -0700

    clicking interface

--- court_dates/api/views.py
+++ court_dates/api/views.py
@@ -23,8 +23,11 @@ def answer_question(request, question_id, answer_id):
     current_question = get_object_or_404(Question, id=question_id)
     answer = get_object_or_404(Answer, id=answer_id)
     next_question = current_question.respond(answer)
-    next_url = reverse('ask question', kwargs={'question_id': next_question.id})
+    # next_url = reverse('ask question', kwargs={'question_id': next_question.id})
+    # data = json.dumps({
+    #     'next': next_url
+    # })
     data = json.dumps({
-        'next': next_url
+        'id': next_question.id
     })
     return HttpResponse(data)
--- court_dates/questions/static/questions/js/custom.js
+++ court_dates/questions/static/questions/js/custom.js
@@ -7,24 +7,50 @@
 const JSONmockupPath = '{"question": "How many ducks?","answer": {"1":"All of em!","2":"Some of em!","3":"None of em!"}}'; 
 const JSONmockup = JSON.parse(JSONmockupPath);
 
+let currentQuestionID = 1; // holds "state" of the current question; will change TODO remove hardcoded
 
-function loadQuestionData(data) {
+function loadQuestionData(data) { 
     const question_text = data.question;
-    const unparsed_answers = data.answer;
+    const unparsed_answers = data.answers;
 
-    let answer_array = new Array();
-
-    for (const key in unparsed_answers) {
-        answer_array.push('<a class="btn btn-primary" href="#" data-id="' + key + '">' + unparsed_answers[key] + '</a>');
-    }
-    const answer_buttons = answer_array.join('<br>');
 
     document.getElementById('question').innerHTML = question_text;
-    document.getElementById('answers').innerHTML = answer_buttons;
+
+    $('#answers').html('');
+    for (const key in unparsed_answers) {
+        const new_button = $('<a class="btn btn-primary" href="#" data-id="' + key + '">' + unparsed_answers[key] + '</a>');
+        new_button.click(sendAnswer);
+        $('#answers').append(new_button);
+    }
 }
 
+function setQuestion(questionID) {
+    currentQuestionID = questionID;
+    // send an api requiest 
+    // load question data with the results 
+    const url = '/api/ask/' + currentQuestionID;
+    console.log(url);
+    $.getJSON(url, loadQuestionData);
+}
 
-window.onload = function(){
-    loadQuestionData(JSONmockup)
-};
+function sendAnswer(event) {
+    event.preventDefault();
+    const target = event.target;
+    const answerId = target.dataset.id;
+    const url = '/api/ask/' + currentQuestionID + '/answer/' + answerId;
+    $.getJSON(url, function(data) {
+        setQuestion(data.id);
+    });
+}
+$(function(){
+    setQuestion(currentQuestionID);
+});
+
+// TODO 
+// set the question ID 
+// call LoadQuestionData based on the question ID
+// for each button: 
+// give it a listener
+// when you click the button 
+// send its data-id as an answer to the current question 
 
