e84150dc20d00da781673ab3819ee058ddacaa1c
EMCain <>
2018-05-18 14:40:57 -0700

    refactor js

--- court_dates/questions/static/questions/js/custom.js
+++ court_dates/questions/static/questions/js/custom.js
@@ -4,24 +4,27 @@
  * and open the template in the editor.
  */
 
-var JSONmockupPath = '{"question": "How many ducks?","answer": {"1":"All of em!","2":"Some of em!","3":"None of em!"}}'; 
-var JSONmockup = JSON.parse(JSONmockupPath);
-var question_text = JSONmockup.question;
-var unparsed_answers = JSONmockup.answer;
+const JSONmockupPath = '{"question": "How many ducks?","answer": {"1":"All of em!","2":"Some of em!","3":"None of em!"}}'; 
+const JSONmockup = JSON.parse(JSONmockupPath);
 
-var answer_array = new Array();
 
-for (var key in unparsed_answers) {
-    answer_array.push('<a class="btn btn-primary" href="#" data-id="' + key + '">' + unparsed_answers[key] + '</a>');
+function loadQuestionData(data) {
+    const question_text = data.question;
+    const unparsed_answers = data.answer;
+
+    let answer_array = new Array();
+
+    for (const key in unparsed_answers) {
+        answer_array.push('<a class="btn btn-primary" href="#" data-id="' + key + '">' + unparsed_answers[key] + '</a>');
+    }
+    const answer_buttons = answer_array.join('<br>');
+
+    document.getElementById('question').innerHTML = question_text;
+    document.getElementById('answers').innerHTML = answer_buttons;
 }
-
-var answer_buttons = answer_array.join('<br>');
-
-
 
 
 window.onload = function(){
-    document.getElementById('question').innerHTML = question_text;
-    document.getElementById('answers').innerHTML = answer_buttons;
+    loadQuestionData(JSONmockup)
 };
 
